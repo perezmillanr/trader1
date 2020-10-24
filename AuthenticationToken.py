99c55273fd59c0b0c8c46db2ba28dde11c1c187e
Rodrigo <perezmillan.r@gmail.com>
2020-10-23 23:57:40 -0300

    Solve conflics

--- AuthenticationToken.py
+++ AuthenticationToken.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import requests
-import json
-
-
-class AuthenticationToken:
-	def __init__(self,username,password):
-		endpoint= "https://api.invertironline.com/token"
-		header={"Content-Type":"application/x-www-form-urlencoded"}
-		param={'username':username,'password':password,"grant_type":"password"}
-
-		HTTSrespose=requests.post(endpoint, data = param, headers = header)
-
-		response=json.loads(HTTSrespose.text)
-
-		self.bearerToken=response["access_token"]
-		self.refreshToken=response["refresh_token"]
-		self.expirationinseconds=response["expires_in"]
-		self.issuedate=response[".issued"]
-		self.expirationdate=response[".expires"]
-
-		print(self.expirationdate)
-
-	def getRefreshToken(self):
-		pass
-	def getBearerToken(self):
-		return self.bearerToken
-	def getRefreshToken(self):
-		return self.refreshToken
-=======
-
 import requests
 import json
 import pickle
@@ -109,4 +78,3 @@ class AuthenticationToken:
                 pickle.dump(tk_obj,token_f)
                 
             return self.bearerToken
->>>>>>> qramosveliz-patch-1
--- trader1.py
+++ trader1.py
@@ -2,14 +2,10 @@
 #2020
 #Invertironline v2 API
 
-<<<<<<< HEAD
 import requests
 import json
 import sys
-import AuthenticationToken
-=======
 import AuthenticationToken as AuthTK
->>>>>>> qramosveliz-patch-1
 from IOL_Operations import *
 from OtherFunctions import *
 
@@ -24,11 +20,7 @@ if __name__ == "__main__":
 		password=GetCredentialsCLI()["pwd"]
 		try:
 			#Genero token
-<<<<<<< HEAD
-			token=AuthenticationToken.AuthenticationToken(username,password)
-=======
 			token=AuthTK.AuthenticationToken(token_dict = AuthTK.requestToken(username,password))
->>>>>>> qramosveliz-patch-1
 			token_ok = True
 		except KeyError:
 			print('Error de token. Reingrese los datos')
@@ -38,11 +30,7 @@ if __name__ == "__main__":
 		password = input('Contraseña: ')
 		try:
 			#Genero token
-<<<<<<< HEAD
-			token=AuthenticationToken.AuthenticationToken(username,password)
-=======
 			token=AuthTK.AuthenticationToken(token_dict = AuthTK.requestToken(username,password))
->>>>>>> qramosveliz-patch-1
 			token_ok = True
 		except KeyError:
 			print('Error de token. Reingrese los datos')
