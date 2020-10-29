8cd343decaf08fa1ba81296e45db931a157455e2
Rodrigo <perezmillan.r@gmail.com>
2020-10-29 20:59:23 -0300

    - New menu with commands - New IOL Functions, like delete order, get order, comprar

--- IOL_Operations.py
+++ IOL_Operations.py
@@ -1,28 +1,30 @@
 import requests
-def getInstrumento_FIXME():
+import json
+import datetime
+#def getInstrumento_FIXME():
 	"""Es una verga, no trae nada copado"""
 
-	mercado="BYMA"
-	especie="PAMP"
-	plazo='t0'
+#	mercado="BYMA"
+#	especie="PAMP"
+#	plazo='t0'
 
-	baseurl="https://api.invertironline.com"
-	endpoint=baseurl+"/api/v2/argentina/Titulos/Cotizacion/Instrumentos"
+#	baseurl="https://api.invertironline.com"
+#	endpoint=baseurl+"/api/v2/argentina/Titulos/Cotizacion/Instrumentos"
 	#endpoint=baseurl+"/api/v2/estadocuenta" #Estado de mi cuenta
-	header={"Accept":"application/json",
-	        'Authorization':"Bearer "+token.getBearerToken()
-	        }
+#	header={"Accept":"application/json",
+#	        'Authorization':"Bearer "+token.getBearerToken()
+#	        }
 
-	param={ 'api_key':token.getBearerToken()}
+#	param={ 'api_key':token.getBearerToken()}
 
-	print(header)
-	print(param)
-	print(endpoint)
+#	print(header)
+#	print(param)
+#	print(endpoint)
 
-	x=requests.get(endpoint,data=param,headers=header)
+#	x=requests.get(endpoint,data=param,headers=header)
 
 
-def getCotizacion(token,especie,plazo):
+def getCotizacion(token,especie,plazo,style="raw"):
 	"""Trae la cotizacion de una especia en BCBA"""
 	baseurl="https://api.invertironline.com"
 	endpoint=baseurl+"/api/v2/bCBA/Titulos/"+especie+"/Cotizacion"
@@ -35,8 +37,14 @@ def getCotizacion(token,especie,plazo):
 			'model.mercado':'bCBA',
 			'model.plazo':plazo,
 			'api_key':token.getBearerToken()}
-	return requests.get(endpoint,data=param,headers=header)
 
+	#Dependiendo del estido damos consas diferentes
+	if style=="raw":
+		return requests.get(endpoint,data=param,headers=header)
+	if style=="ultimoprecio":
+		return json.loads(requests.get(endpoint,data=param,headers=header).text)["ultimoPrecio"]
+	else:
+		return requests.get(endpoint,data=param,headers=header)
 
 def getEstadoCuenta(token):
 	"""Devuelve el estado de la cuenta"""
@@ -50,4 +58,58 @@ def getEstadoCuenta(token):
 
 	return requests.get(endpoint,data=param,headers=header)
 
+#def getOperacion_FIXME(token,id):
+	"""Devuelve el estado de una operación"""
+#	baseurl="https://api.invertironline.com"
+#	endpoint=baseurl+"/api/v2/operacion/"+str(id)
+#	header={"Accept":"application/json",
+#	        'Authorization':"Bearer "+token.getBearerToken()
+#	        }
+#	param={'api_key':token.getBearerToken()}
+#	return requests.get(endpoint,data=param,headers=header)
 
+
+def delOperacion(token,id):
+	"""Borra una operación"""
+	baseurl="https://api.invertironline.com"
+	endpoint=baseurl+"/api/v2/operaciones/"+str(id)
+	header={"Accept":"application/json",
+	        'Authorization':"Bearer "+token.getBearerToken()
+	        }
+	param={'api_key':token.getBearerToken()}
+
+	print(endpoint)
+	print(header)
+	return requests.delete(endpoint,data=param,headers=header)
+
+
+def getOperaciones(token):
+	"""Devuelve las operaciones"""
+	baseurl="https://api.invertironline.com"
+	endpoint=baseurl+"/api/v2/operaciones"
+	header={"Accept":"application/json",
+	        'Authorization':"Bearer "+token.getBearerToken()
+	        }
+	param={'api_key':token.getBearerToken()}
+	return requests.get(endpoint,data=param,headers=header)
+
+
+def comprar(token,especie,plazo,precio,cantidad,validez=24):
+	"""Pone una orden de compra"""
+
+	d=datetime.datetime.now()+datetime.timedelta(days=validez)
+
+	baseurl="https://api.invertironline.com"
+	endpoint=baseurl+"/api/v2/operar/Comprar"
+	header={"Accept":"application/json",
+	        'Authorization':"Bearer "+token.getBearerToken()
+	        }
+
+	param={ 'simbolo':especie,
+			'mercado':'bCBA',
+			'plazo':plazo,
+			'cantidad':cantidad,
+			'precio':precio,
+			'validez': d.isoformat('T',"milliseconds")+"Z",
+			'api_key':token.getBearerToken()}
+	return requests.post(endpoint,data=param,headers=header)
--- trader1.py
+++ trader1.py
@@ -35,25 +35,75 @@ if __name__ == "__main__":
 		except KeyError:
 			print('Error de token. Reingrese los datos')
 
-
+	print("Bienvenido")
+	print('Ingrese comando ("help" para ver los comandos):')
 	while True:
-		accion = input('Ingrese Comando ("help" para ver los comandos): ')			
-
-		if accion.lower()=="cot":
-			especie = input('Ingrese el especie (n para salir): ')
+		comando = input('>> ')			
+		if comando.lower()=="cot":
+			especie = input('Ingrese el especie (n para cancelar): ')
 			if especie == 'n':	
 				continue
 			else:
-				t_ = input('Ingrese el tiempo (t0,t1,t2): ')
-				print(getCotizacion(token,accion,t_))
-				print(getCotizacion(token,accion,t_).text)
+				t_ = input('Ingrese plazo (t0,t1,t2): ')
+				print(getCotizacion(token,especie,t_).text)
 				continue
-		if accion.lower()=="help":
+		if comando.lower()=="cotp":
+			especie = input('Ingrese el especie (n para cancelar): ')
+			if especie == 'n':	
+				continue
+			else:
+				t_ = input('Ingrese plazo (t0,t1,t2): ')
+				print(getCotizacion(token,especie,t_,style="ultimoprecio"))
+				continue
+
+
+		if comando.lower()=="del":
+			operacion = input('Ingrese la orden (n para cancelar): ')
+			if operacion == 'n':	
+				continue
+			else:
+				print(delOperacion(token,operacion))
+				continue
+
+		if comando.lower()=="comprar":
+			especie = input('Ingrese el especie (n para cancelar): ')
+			if especie == 'n':	
+				continue
+			plazo = input('Ingrese plazo (t0,t1,t2) (n para cancelar):')
+			if plazo == 'n':	
+				continue
+			precio = input('Ingrese el precio (n para cancelar): ')
+			if precio == 'n':	
+				continue
+			cantidad = input('Ingrese cantidad (n para cancelar):')
+			if cantidad == 'n':	
+				continue
+
+			print(comprar(token,especie,plazo,precio,cantidad).text)
+			continue
+
+		if comando.lower()=="estado":
+				print(getEstadoCuenta(token).text)
+				continue
+
+		if comando.lower().startswith("getop"):
+			if comando.lower()=="getop":
+				print(getOperaciones(token).text)
+			else:
+				id=int(comando.split()[-1])
+				print(id)
+			continue	
+		if comando.lower()=="help":
 			print("Comandos:")
 			print("cot - Obtener Cotizacion de una Especie")
+			print("cotp - Obtener ultimo precio de una Especie")
+			print("comprar - Colocar orden de compra")
+			print("del - Borrar una orden")
+			print("estado - Obtener Estado de cuenta")
 			print("salir - Salir de la aplicación")
 			print("help - Obtener Lista de Comandos")
-		if accion.lower()=="salir":
+			continue
+		if comando.lower()=="salir":
 			break
 		print ("Comando no reconocido")
 
