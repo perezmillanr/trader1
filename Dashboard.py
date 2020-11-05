6f966d6727b46666f93026a017ca64c3f016db66
Rodrigo <perezmillan.r@gmail.com>
2020-11-05 12:32:23 -0300

    New changes on the dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -1,2 +1,27 @@
+import json
+
 class Dashboard:
+	def __init__(self,name="Default Dashboard"):
+		self.name=name
+		self.path="/dashboard.json"
+		pass
+	def __repr__(self):
+		"""Aca vamos a printear el Dashboard """
+		pass
+	def addEspecie(self):
+		"""Añaadir una especie al Dashbaord"""
+		pass
+	def removeEspecie(self):
+		"""Remover una especie al Dashbaord"""
+		pass
+	def addExpression(self):
+		"""Añadir una expresion al Dashbaord"""
+		pass
+	def __saveDashboard(self):
+		"""Guarda el Dashbaord en el file"""
+		pass
+	def __loadDhasboard(self):
+		"""Levanta el Dashboard en el file"""
+		pass
+
 	pass
